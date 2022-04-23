4e9d18ccd8436679b73abc6203165071417dde15
GPMrks <guilhermepereiramarques@hotmail.com>
2022-04-23 13:31:45 -0300

    DOC - Init. Uploading files.

---
+++ src/main/java/one/digitalinnovation/TelegramClient/TelegramClientApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.TelegramClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/TelegramClient/TelegramClientApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.TelegramClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
